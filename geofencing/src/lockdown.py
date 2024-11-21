99a7f23fdfbf5d3819f4d2071eb8a204b692dd17
vaibhavanuragi2004 <vaibhavanuragi02@gmail.com>
2024-11-21 12:03:33 +0530

    Create lockdown.py

---
+++ geofencing/src/lockdown.py
@@ -0,0 +1,7 @@
+def activate_lockdown(vehicle_id):
+    """
+    Activates the lockdown procedure for the vehicle.
+    :param vehicle_id: Unique ID of the vehicle.
+    """
+    print(f"Vehicle {vehicle_id} is locked down remotely.")
+    # Placeholder for integration with the vehicle's API.
